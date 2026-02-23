2c37fc5fe96ec0aece785254ba2b302e12fd99c0
Eren Balcı <me@erenbalci.com>
2026-02-23 22:15:25 +0100

    version 0.1.0

--- cmd/protoc-gen-connect-errors-go/main.go
+++ cmd/protoc-gen-connect-errors-go/main.go
@@ -18,7 +18,7 @@ import (
 	"google.golang.org/protobuf/types/descriptorpb"
 )
 
-var version = "0.2.0"
+var version = "0.1.0"
 
 func main() {
 	showVersion := flag.Bool("version", false, "print version")
